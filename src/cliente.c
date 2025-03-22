01ba65f1e6037f8a427721df32a7ca3f37c96462
Ing-Adrian-RM <adri1510.rm@gmail.com>
2025-03-21 21:27:09 -0600

    Update to cliente.c

--- src/cliente.c
+++ src/cliente.c
@@ -10,13 +10,17 @@
 #define BUFFER_SIZE 1024
 #define SERVER "192.168.1.105"
 
-int client_socket; // Global variable for the thread
+int client_socket, line=1; // Global variable for the thread
+int max_width;
+int BUFFER_SEND_SIZE;
+char *temp = NULL;
 WINDOW *chat_win, *input_win;
+pthread_mutex_t lock;
 
 // Thread to receive messages from the server
 void *receive_messages() {
     char buffer[BUFFER_SIZE];
-    int line = 1; // Start at the first line inside the chat window
+    line = 1; // Start at the first line inside the chat window
 
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
@@ -26,41 +30,72 @@ void *receive_messages() {
             wprintw(chat_win, "Server disconnected\n");
             wrefresh(chat_win);
             close(client_socket);
-            endwin(); // Close ncurses
+            endwin();
             exit(0);
         }
 
-        // Print the message at the current line
-        mvwprintw(chat_win, line, 1, "Server: %s", buffer);
-        wrefresh(chat_win);
+        char *message = buffer;
+        int lines_writed = 1;
+        while (*message) {
+            if (line >= LINES - 5) { // Leave space for the input window
+                box(chat_win,' ',' ');
+                scroll(chat_win);
+                box(chat_win, 0, 0);
+                pthread_mutex_lock(&lock);
+                line--;
+                pthread_mutex_unlock(&lock); 
+            }
+            
+            memset(temp, ' ', sizeof(max_width + 1));
+            if (lines_writed == 1){
+                strncpy(temp, message, max_width - 8);
+                temp[max_width + 1] = '\0';
+                mvwprintw(chat_win, line, 1, "Server: %s", temp);
+                message += (max_width - 8);
+            } else if (lines_writed > 1){
+                strncpy(temp, message, max_width);
+                temp[max_width + 1] = '\0';
+                mvwprintw(chat_win, line, 1, "%s", temp);
+                message += (max_width);
+            }            
+            
+            box(chat_win, 0, 0);
+            wrefresh(chat_win);
+            lines_writed++;
 
-        // Move to the next line
-        line++;
-
-        // If the window is full, scroll up
-        if (line >= LINES - 4) { // Adjust for borders and input window
-            scroll(chat_win);
-            line = LINES - 5; // Keep the last line for new messages
+            pthread_mutex_lock(&lock);
+            line++;
+            pthread_mutex_unlock(&lock);
         }
+
+        wmove(input_win, sizeof("Message: "), 1);
+        wrefresh(input_win);
     }
     return NULL;
 }
 
-int main() {
+void setup() {
     struct sockaddr_in server_address;
-    char buffer[BUFFER_SIZE];
+    pthread_mutex_init(&lock, NULL);
 
     // Initialize ncurses
     initscr();
     cbreak();
-    //noecho();
     curs_set(1);
+    max_width = COLS - 2;
+    BUFFER_SEND_SIZE = COLS * 2;
+    temp = malloc((max_width + 1) * sizeof(char));
+    if (temp == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
 
     // Create windows for chat and input
     int height = LINES - 4;
     int width = COLS;
     chat_win = newwin(height, width, 0, 0);
     input_win = newwin(4, width, height, 0);
+    scrollok(chat_win, TRUE);
 
     // Draw borders
     box(chat_win, 0, 0);
@@ -93,34 +128,88 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to the server. Type 'exit' to terminate.\n");
+    mvwprintw(chat_win, line, 1, "Connected to the server. Type 'exit' to terminate.\n");
+    wrefresh(chat_win);
+    wmove(input_win, 1, sizeof("Message: "));
+    wrefresh(input_win);
+    pthread_mutex_lock(&lock);
+    line++;
+    pthread_mutex_unlock(&lock);
 
     // Create a thread to receive messages from the server
     pthread_t reception_thread;
     pthread_create(&reception_thread, NULL, receive_messages, NULL);
+}
+
+int main() {
+    setup();
+    char buffer[BUFFER_SEND_SIZE];
 
     // Loop to send messages
     while (1) {
         werase(input_win);
-        box(input_win, 0, 0);
-        //fgets(buffer, BUFFER_SIZE, stdin);
-        mvwprintw(input_win, 1, 1, "Message: ");
+        wborder(input_win, ' ', ' ', '-', '-', '-', '-', '-', '-');
+        mvwprintw(input_win, 1, 0, "Message: ");
         wrefresh(input_win);
-        wgetnstr(input_win, buffer, BUFFER_SIZE);
+        memset(buffer, 0, BUFFER_SEND_SIZE);
+        wgetnstr(input_win, buffer, BUFFER_SEND_SIZE - 9);
         buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
-
+        
         send(client_socket, buffer, strlen(buffer), 0);
+        
+        char *message = buffer;
+        int lines_writed = 1;
+        while (*message) {
+            if (line >= LINES - 5) {
+                box(chat_win,' ',' ');
+                scroll(chat_win);
+                box(chat_win, 0, 0);
+                pthread_mutex_lock(&lock);
+                line--;
+                pthread_mutex_unlock(&lock);
+            }
 
+            memset(temp, ' ', sizeof(max_width + 1));
+            if (lines_writed == 1){
+                strncpy(temp, message, max_width - 4);
+                temp[max_width + 1] = '\0';
+                mvwprintw(chat_win, line, 1, "Me: %s", temp);
+                message += (max_width - 4);
+            } else if (lines_writed > 1){
+                strncpy(temp, message, max_width);
+                temp[max_width + 1] = '\0';
+                mvwprintw(chat_win, line, 1, "%s", temp);
+                message += (max_width);
+            }            
+            
+            box(chat_win, 0, 0);
+            wrefresh(chat_win);
+            lines_writed++;
+
+            pthread_mutex_lock(&lock);
+            line++;
+            pthread_mutex_unlock(&lock);
+        }
+
+        wmove(input_win, sizeof("Message: "), 1);
+        wrefresh(input_win);
+        
         if (strncmp(buffer, "exit", 4) == 0) {
-            //printf("Closing connection...\n");
             wprintw(chat_win, "Closing connection...\n");
             wrefresh(chat_win);
             close(client_socket);
             endwin(); // Close ncurses
             exit(0);
-        }
+        } else if (strncmp(buffer, "clear", 5) == 0) {
+            werase(chat_win);
+            box(chat_win, 0, 0);
+            wrefresh(chat_win);
+            line = 1;
+        }   
     }
 
+    free(temp);
+    pthread_mutex_destroy(&lock);
     endwin(); // Close ncurses
     return 0;
 }
